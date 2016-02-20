887045d361cade0804217c53eeb378801238f748
Tal Ater <tal@talater.com>
2016-02-20 15:55:57 +0200

    Choosing your own adventure now sets your choices in a choices object

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,20 @@
 (function() {
+  var choices = {
+    type: 'issues',
+    issue_types: 'bug-or-feature',
+    environment: 'browser-and-server'
+  };
+
   $('.page-turner').click(function(ev) {
-    var pageNumber = $(ev.currentTarget).data('targetPage');
+    var $target = $(ev.currentTarget);
+
+    var choiceKey = $target.data('choiceKey');
+    var choiceValue = $target.data('choiceValue');
+    if (choiceKey && choiceValue) {
+      choices[choiceKey] = choiceValue;
+    }
+
+    var pageNumber = $target.data('targetPage');
     $('body').scrollTop(0);
     $('.cover').hide();
     $('.page').removeClass('page--current');
