8333e9c3133080013f1fb298ecbe4625065e47df
Tal Ater <tal@talater.com>
2016-02-20 14:23:44 +0200

    Added pages: cover, 38, 39, 42. Edited other pages. Improved functionality of page turning

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,8 @@
 (function() {
   $('.page-turner').click(function(ev) {
     var pageNumber = $(ev.currentTarget).data('targetPage');
+    $('body').scrollTop(0);
+    $('.cover').hide();
     $('.page').removeClass('page--current');
     $('#page-'+pageNumber).addClass('page--current');
     return false;
