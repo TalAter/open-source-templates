8a0e631c7ede7a2fa64bbbf3bf752e2d80314698
Tal Ater <tal@talater.com>
2016-02-20 16:14:00 +0200

    Completed functionality to build the ISSUE_TEMPLATE.md file

--- site/js/main.js
+++ site/js/main.js
@@ -1,20 +1,29 @@
 (function() {
   var choices = {
     type: 'issues',
-    issue_types: 'bug-or-feature',
+    description: 'bug-or-feature',
     environment: 'browser-and-server'
   };
 
   $('.page-turner').click(function(ev) {
     var $target = $(ev.currentTarget);
-
+    var pageNumber = $target.data('targetPage');
     var choiceKey = $target.data('choiceKey');
     var choiceValue = $target.data('choiceValue');
-    if (choiceKey && choiceValue) {
-      choices[choiceKey] = choiceValue;
+
+    if (pageNumber === 1) {
+      $('textarea').val('');
     }
 
-    var pageNumber = $target.data('targetPage');
+    if (choiceKey && choiceValue) {
+      choices[choiceKey] = choiceValue;
+      if (choiceKey === 'description' || choiceKey === 'environment') {
+        $.get( "templates/issue/"+choiceKey+"/"+choiceValue+".md", function(text) {
+          $("textarea").val($("textarea").val()+"\n"+text);
+        });
+      }
+    }
+
     $('body').scrollTop(0);
     $('.cover').hide();
     $('.page').removeClass('page--current');
