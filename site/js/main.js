37c82ef31906e23d8c8d6e41898b8eef098bfa45
Tal Ater <tal@talater.com>
2016-02-21 22:29:18 +0200

    Implemented a router

--- site/js/main.js
+++ site/js/main.js
@@ -5,29 +5,34 @@
     environment: 'browser-and-server'
   };
 
-  $('.page-turner').click(function(ev) {
-    var $target = $(ev.currentTarget);
-    var pageNumber = $target.data('targetPage');
-    var choiceKey = $target.data('choiceKey');
-    var choiceValue = $target.data('choiceValue');
-
+  var viewPage = function(pageNumber) {
     if (pageNumber === 1) {
       $('textarea').val('');
     }
-
-    if (choiceKey && choiceValue) {
-      choices[choiceKey] = choiceValue;
-      if (choiceKey === 'description' || choiceKey === 'environment') {
-        $.get( "templates/issue/"+choiceKey+"/"+choiceValue+".md", function(text) {
-          $("textarea").val($("textarea").val()+"\n"+text);
-        });
-      }
-    }
-
     $('body').scrollTop(0);
     $('.cover').hide();
     $('.page').removeClass('page--current');
     $('#page-'+pageNumber).addClass('page--current');
-    return false;
-  });
+  };
+
+  var makeChoice = function(pageNumber, choiceKey, choiceValue) {
+    choices[choiceKey] = choiceValue;
+    if (choiceKey === 'description' || choiceKey === 'environment') {
+      $.get( "templates/issue/"+choiceKey+"/"+choiceValue+".md", function(text) {
+        $("textarea").val($("textarea").val()+"\n"+text);
+      });
+    }
+
+    return viewPage(pageNumber);
+  };
+
+  var routes = {
+    '/page/:pageNumber': viewPage,
+    '/page/:pageNumber/:choiceKey/:choiceValue': makeChoice
+  };
+
+  var router = Router(routes);
+
+  router.init();
+
 })()
