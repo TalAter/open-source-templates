ba7ee28a5092b5333bcf291ac810deb7c70bacae
Tal Ater <tal@talater.com>
2016-02-22 16:26:42 +0200

    Pull requests template builds succesfully

--- site/js/main.js
+++ site/js/main.js
@@ -11,36 +11,65 @@
   };
 
   var makeChoice = function(pageNumber, choiceKey, choiceValue) {
-    choices[choiceKey] = choiceValue;
-
-    if (choiceKey === 'description' || choiceKey === 'environment') {
-      var contentKey = choiceKey+'-'+choiceValue;
-      if (content[contentKey] === undefined) {
-        $.get( "templates/"+choices['type']+"/"+choiceKey+"/"+choiceValue+".md", function(text) {
-          content[contentKey] = text;
-          updateTextarea();
-        });
-      }
+    if (pageNumber === 1) {
+      choices = {};
     }
 
-    updateTextarea();
+    choices[choiceKey] = choiceValue;
+
+    fetchContent(choiceKey, choiceValue);
 
     return viewPage(pageNumber);
   };
 
-  var updateTextarea = function() {
-    if (choices['type'] === 'issues') {
-      $('textarea').val(
-        content['description-'+choices['description']]+"\n"+
-        content['environment-'+choices['environment']]
-        );
+  var addToChecklist = function(pageNumber, choiceValue) {
+    choices['checklist'] = choices['checklist'] || {};
+
+    choices['checklist'][choiceValue] = choiceValue;
+
+    fetchContent('checklist', choiceValue);
+
+    return viewPage(pageNumber);
+  };
+
+  var fetchContent = function(choiceKey, choiceValue, type) {
+    type = type || choices['type'];
+    if (['description', 'environment', 'checklist', 'change-types'].indexOf(choiceKey) !== -1) {
+      var contentKey = type+'-'+choiceKey+'-'+choiceValue;
+      if (content[contentKey] === undefined) {
+        $.get( "templates/"+type+"/"+choiceKey+"/"+choiceValue+".md", function(text) {
+          content[contentKey] = text;
+          updateTextarea();
+        });
+      } else {
+        updateTextarea();
+      }
     }
+  };
+
+  var updateTextarea = function() {
+    var text = '';
+    if (choices['type'] === 'issues') {
+      text =  content['issues-description-'+choices['description']]+"\n"+
+              content['issues-environment-'+choices['environment']];
+    } else if (choices['type'] === 'pull-requests') {
+      text  = content['pull-requests-description-'+choices['description']]+"\n"+
+              content['pull-requests-change-types-bug-or-feature']+"\n"+
+              content['pull-requests-checklist-intro'];
+      ['contributing', 'tests'].forEach(function(checklistItem) {
+        if (choices['checklist'] && choices['checklist'][checklistItem]) {
+          text += content['pull-requests-checklist-'+checklistItem];
+        }
+      });
+    }
+    $('textarea').val(text);
   };
 
   $('.page').hide();
 
   var routes = {
     '/page/:pageNumber': viewPage,
+    '/page/:pageNumber/checklist/:choiceValue': addToChecklist,
     '/page/:pageNumber/:choiceKey/:choiceValue': makeChoice
   };
 
@@ -49,5 +78,8 @@
   router.setRoute('/')
 
   router.init();
+
+  fetchContent('change-types', 'bug-or-feature', 'pull-requests');
+  fetchContent('checklist', 'intro', 'pull-requests');
 
 })()
